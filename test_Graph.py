87cc7ea394e085d3c8899ad6d2f788c387c43f1a
Renich Bon Ciric <renich@woralelandia.com>
2012-10-18 23:08:27 -0500

    Agregué una prueba para ver si la arista fue eliminada

--- test_Graph.py
+++ test_Graph.py
@@ -3,23 +3,26 @@ from Graph import Graph, Vertex, Edge
 
 class GraphTest(unittest.TestCase):
 
-	def setUp(self):
-		self.v = Vertex('v')
-		self.w = Vertex('w')
-		self.x = Vertex('x')
-		self.y = Vertex('y')
-		self.z = Vertex('z')
-		self.e = Edge(self.v, self.w)
-		self.e2 = Edge(self.v, self.x)
-		self.g = Graph([self.v, self.w ,self.x, self.y],[self.e, self.e2])
+    def setUp(self):
+        self.v = Vertex('v')
+        self.w = Vertex('w')
+        self.x = Vertex('x')
+        self.y = Vertex('y')
+        self.z = Vertex('z')
+        self.e = Edge(self.v, self.w)
+        self.e2 = Edge(self.v, self.x)
+        self.g = Graph([self.v, self.w ,self.x, self.y],[self.e, self.e2])
 
-	def test_get_edge_not_exist(self):
-	    edge = self.g.get_edge(self.v, self.y)
-	    self.assertIsNone(edge)
+    def test_remove_edge_is_removed( self ):
+        '''
+        La prueba checa si el Edge esta por ahi.
+
+        Depende de la funcion de get_edge
+        '''
+        edge = self.g.remove_edge( self.e )
+        self.assertIsNone( self.g.get_edge( self.v, self.w ) )
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     unittest.TextTestRunner(verbosity = 2).run(suite)
     unittest.main()
-
-
