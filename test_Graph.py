b27c47d47983d16d632614531d8bb9b63f55911d
celakanto <celakanto@linux-91wy.site>
2012-10-18 22:10:48 -0500

    Agregada prueba de get_edge

--- test_Graph.py
+++ test_Graph.py
@@ -1,4 +1,5 @@
 import unittest
+from Graph import Graph, Vertex, Edge
 
 class GraphTest(unittest.TestCase):
 
@@ -11,5 +12,14 @@ class GraphTest(unittest.TestCase):
 		self.e = Edge(self.v, self.w)
 		self.e2 = Edge(self.v, self.x)
 		self.g = Graph([self.v, self.w ,self.x, self.y],[self.e, self.e2])
+
+	def test_get_edge_not_exist(self):
+	    edge = self.g.get_edge(self.v, self.y)
+	    self.assertIsNone(edge)
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    unittest.TextTestRunner(verbosity = 2).run(suite)
+    unittest.main()
 
 
