e57106684e3d45ec61f5ac9bd2876633217d9628
celakanto <celakanto@linux-91wy.site>
2012-10-18 22:49:47 -0500

    Agregada prueba de get_edge_exist

--- test_Graph.py
+++ test_Graph.py
@@ -13,13 +13,15 @@ class GraphTest(unittest.TestCase):
         self.e2 = Edge(self.v, self.x)
         self.g = Graph([self.v, self.w ,self.x, self.y],[self.e, self.e2])
 
-        def test_get_edge_not_exist(self):
-            edge = self.g.get_edge(self.v, self.y)
-            self.assertIsNone(edge)
+    def test_get_edge_not_exist(self):
+        edge = self.g.get_edge(self.v, self.y)
+        self.assertIsNone(edge)
+
+    def test_get_edge_exist(self):
+        edge = self.g.get_edge(self.v, self.w)
+        self.assertEqual(edge, self.e)
 
 if __name__ == '__main__':
-    suite = unittest.TestSuite()
-    unittest.TextTestRunner(verbosity = 2).run(suite)
     unittest.main()
 
 
