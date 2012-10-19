1f2c0831259e08a53423cb31b53b8dee66194bb7
celakanto <celakanto@linux-91wy.site>
2012-10-18 22:24:51 -0500

    Tabs a epacios

--- test_Graph.py
+++ test_Graph.py
@@ -3,19 +3,19 @@ from Graph import Graph, Vertex, Edge
 
 class GraphTest(unittest.TestCase):
 
-	def setUp(self):
-		self.v = Vertex('v')
-		self.w = Vertex('w')
-		self.x = Vertex('x')
-		self.y = Vertex('y')
-		self.z = Vertex('z')
-		self.e = Edge(self.v, self.w)
-		self.e2 = Edge(self.v, self.x)
-		self.g = Graph([self.v, self.w ,self.x, self.y],[self.e, self.e2])
+    def setUp(self):
+        self.v = Vertex('v')
+        self.w = Vertex('w')
+        self.x = Vertex('x')
+        self.y = Vertex('y')
+        self.z = Vertex('z')
+        self.e = Edge(self.v, self.w)
+        self.e2 = Edge(self.v, self.x)
+        self.g = Graph([self.v, self.w ,self.x, self.y],[self.e, self.e2])
 
-	def test_get_edge_not_exist(self):
-	    edge = self.g.get_edge(self.v, self.y)
-	    self.assertIsNone(edge)
+        def test_get_edge_not_exist(self):
+            edge = self.g.get_edge(self.v, self.y)
+            self.assertIsNone(edge)
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
